51d8463425aba65ff48fc7d5130b00b0ac28cab4
Ana Aguilera <anitaaguilera@hotmail.com>
2026-07-28 15:02:05 +0200

    feat: configure router and application layout

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+    import Container from '../../components/common/Container';
+
+    function Home() {
+    return (
+        <main>
+        <Container>
+            <h1>Portfolio Iki</h1>
+        </Container>
+        </main>
+    );
+    }
+
+    export default Home;
